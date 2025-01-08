3a9397218764cbd2245b54a6bd7b4bc144d73366
Volodymyr <vovatsyukh@gmail.com>
2025-01-08 13:01:15 +0200

    user logic started

--- server/src/review/review.controller.ts
+++ server/src/review/review.controller.ts
@@ -1,7 +1,72 @@
-import { Controller } from '@nestjs/common';
-import { ReviewService } from './review.service';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+import { ReviewService } from "./review.service";
+import { CreateReviewDto } from "./dto/createReviewDto";
+import { UpdateReviewDto } from "./dto/updateReviewDto";
 
-@Controller('review')
+@Controller("review")
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @Post()
+  async createReview(@Body() createReviewDto: CreateReviewDto) {
+    try {
+      const review = await this.reviewService.createReview(createReviewDto);
+      return review;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Get("room/:roomId")
+  async getReviewsForRoom(@Param("roomId") roomId: number) {
+    const reviews = await this.reviewService.getReviewsForRoom(roomId);
+    return reviews;
+  }
+
+  @Get("user/:userId")
+  async getReviewsForUser(@Param("userId") userId: number) {
+    const reviews = await this.reviewService.getReviewsForUser(userId);
+    return reviews;
+  }
+
+  @Get()
+  async getAllReviews() {
+    const reviews = await this.reviewService.getAllReviews();
+    return reviews;
+  }
+
+  @Patch(":id")
+  async updateReview(
+    @Param("id") id: number,
+    @Body() updateReviewDto: UpdateReviewDto
+  ) {
+    try {
+      const updatedReview = await this.reviewService.updateReview(
+        id,
+        updateReviewDto
+      );
+      return updatedReview;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @Delete(":id")
+  async deleteReview(@Param("id") id: number) {
+    try {
+      await this.reviewService.deleteReview(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+    }
+  }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import bcrypt from "bcrypt";
+import { CreateUserDto } from "./dto/createUserDto";
+import { UpdateUserDto } from "./dto/updateUserDto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: number) {
+    try {
+      const user = await this.prisma.user.findUnique({ where: { id } });
+      if (!user) {
+        throw new NotFoundException("User not found");
+      }
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException("Error fetching user data");
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.prisma.user.findMany({
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          role: true,
+          createdAt: true,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException("Error fetching all users");
+    }
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const { email, password, name } = createUserDto;
+
+    try {
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email },
+      });
+      if (existingUser) {
+        throw new BadRequestException("Email is already in use");
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      return await this.prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          name,
+        },
+      });
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+      throw new InternalServerErrorException("Error creating user");
+    }
+  }
+
+  async updateUser(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const user = await this.findById(id);
+
+      const { name, photoUrl } = updateUserDto;
+
+      return await this.prisma.user.update({
+        where: { id },
+        data: { name, photoUrl },
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException("Error updating user");
+    }
+  }
+
+  async deleteUser(id: number) {
+    try {
+      await this.findById(id);
+      return await this.prisma.user.delete({ where: { id } });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException("Error deleting user");
+    }
+  }
+}
